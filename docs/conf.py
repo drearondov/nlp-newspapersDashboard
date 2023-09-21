6f7a051c084338bd74adf5b2a411c2766e1d3f39
drearondov <63360808+drearondov@users.noreply.github.com>
2023-09-21 09:22:41 -0500

    first commit

---
+++ docs/conf.py
@@ -0,0 +1,12 @@
+"""Sphinx configuration."""
+project = "NLP Newspapers Dashboard"
+author = "Andrea Rondon"
+copyright = "2023, Andrea Rondon"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_click",
+    "myst_parser",
+]
+autodoc_typehints = "description"
+html_theme = "furo"
---
+++ docs/usage.md
@@ -0,0 +1,7 @@
+# Usage
+
+```{eval-rst}
+.. click:: nlp_newspapersDashboard.__main__:main
+    :prog: nlp-newspapersDashboard
+    :nested: full
+```
